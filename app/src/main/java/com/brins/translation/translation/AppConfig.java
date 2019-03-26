36f594675696b83a2ec5afbd2b5417fadad49026
Brin Lee <747127514@qq.com>
2019-03-26 18:18:57 +0800

    import retrofit

--- app/src/main/java/com/brins/translation/translation/AppConfig.java
+++ app/src/main/java/com/brins/translation/translation/AppConfig.java
@@ -1,7 +1,126 @@
 package com.brins.translation.translation;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class AppConfig {
 
-    public static final String BASEURL = "http://fanyi.youdao.com/";
+    public static final String BASEURL = "https://translate.google.cn/";
+    public static final String USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36";
+    public static final Map<String ,String > languageSelect = languageSelected();
 
+    private static Map<String,String> languageSelected() {
+
+        Map<String, String> m = new HashMap<>();
+        m.put("Automatic","auto");
+        m.put("Afrikaans","af");
+        m.put("Albanian","sq");
+        m.put("Arabic","ar");
+        m.put("Armenian","hy");
+        m.put("Azerbaijani","az");
+        m.put("Basque","eu");
+        m.put("Belarusian","be");
+        m.put("Bengali","bn");
+        m.put("Bosnian","bs");
+        m.put("Bulgarian","bg");
+        m.put("Catalan","ca");
+        m.put("Cebuano","ceb");
+        m.put("Chichewa","ny");
+        m.put("Chinese Simplified","zh-cn");
+        m.put("Chinese Traditional","zh-tw");
+        m.put("Corsican","co");
+        m.put("Croatian","hr");
+        m.put("Czech","cs");
+        m.put("Danish","da");
+        m.put("Dutch","nl");
+        m.put("English","en");
+        m.put("Esperanto","eo");
+        m.put("Estonian","et");
+        m.put("Filipino","tl");
+        m.put("Finnish","fi");
+        m.put("French","fr");
+
+        m.put("Frisian","fy");
+        m.put("Galician","gl");
+        m.put("Georgian","ka");
+        m.put("German","de");
+        m.put("Geek","el");
+        m.put("Gujarati","gu");
+        m.put("Haitian Creole","ht");
+        m.put("Hausa","ha");
+        m.put("Hawaiian","haw");
+        m.put("Hebrew","iw");
+        m.put("Hindi","hi");
+        m.put("Hmong","hmn");
+        m.put("Hungarian","hu");
+        m.put("Icelandic","is");
+        m.put("Igbo","ig");
+        m.put("Indonesian","id");
+        m.put("Irish","ga");
+        m.put("Italian","it");
+        m.put("Japanese","ja");
+        m.put("Javanese","jw");
+        m.put("Kannada","kn");
+        m.put("Kazakh","kk");
+        m.put("Khmer","km");
+
+        m.put("Korean","ko");
+        m.put("Kurdish (Kurmanji)","ku");
+        m.put("Kyrgyz","ky");
+        m.put("Lao","lo");
+        m.put("Latin","la");
+        m.put("Latvian","lv");
+        m.put("Lithuanian","lt");
+        m.put("Luxembourgish","lb");
+        m.put("Macedonian","mk");
+        m.put("Malagasy","mg");
+        m.put("Malay","ms");
+        m.put("Malayalam","ml");
+        m.put("Maltese","mt");
+        m.put("Maori","mi");
+        m.put("Marathi","mr");
+        m.put("Mongolian","mn");
+        m.put("Myanmar (Burmese)","my");
+        m.put("Nepali","ne");
+        m.put("Norwegian","no");
+        m.put("Pashto","ps");
+        m.put("Persian","fa");
+        m.put("Polish","pl");
+        m.put("Portuguese","pt");
+        //
+        m.put("Punjabi","ma");
+        m.put("Romanian","ro");
+        m.put("Russian","ru");
+        m.put("Samoan","sm");
+        m.put("Scots Gaelic","gd");
+        m.put("Serbian","sr");
+        m.put("Sesotho","st");
+
+        m.put("Shona","sn");
+        m.put("Sindhi","sd");
+        m.put("Sinhala","si");
+        m.put("Slovak","sk");
+        m.put("Slovenian","sl");
+        m.put("Somali","so");
+        m.put("Spanish","es");
+        m.put("Sudanese","su");
+        m.put("Swahili","sw");
+        m.put("Swedish","sv");
+        m.put("Tajik","tg");
+        m.put("Tamil","ta");
+        m.put("Telugu","te");
+        m.put("Thai","th");
+        m.put("Turkish","tr");
+        m.put("Ukrainian","uk");
+        m.put("Urdu","ur");
+        m.put("Uzbek","uz");
+        m.put("Vietnamese","vi");
+        m.put("Welsh","cy");
+        m.put("Xhosa","xh");
+        m.put("Yiddish","yi");
+        m.put("Yoruba","yo");
+        m.put("Zulu","zu");
+        return m;
+
+    }
 }
--- app/src/main/java/com/brins/translation/translation/fragment/mainfragment.kt
+++ app/src/main/java/com/brins/translation/translation/fragment/mainfragment.kt
@@ -20,9 +20,6 @@ import com.brins.translation.translation.api.PostRequest_Interface
 import com.brins.translation.translation.model.Translation
 import com.brins.translation.translation.utils.Animation
 import com.brins.translation.translation.utils.ToastUtils
-import com.youdao.sdk.chdict.ChDictTranslate
-import com.youdao.sdk.chdict.ChDictor
-import com.youdao.sdk.chdict.DictListener
 import kotlinx.android.synthetic.main.fragment_main.*
 import retrofit2.Call
 import retrofit2.Callback
@@ -32,8 +29,6 @@ class mainfragment : Fragment(){
 
     var request : PostRequest_Interface? = null
     var call : Call<Translation>? = null
-    private var chDictor: ChDictor? = null
-    internal lateinit var chTslate: List<ChDictTranslate>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -50,7 +45,6 @@ class mainfragment : Fragment(){
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         tv_chinese.setOnClickListener {
-            lookup_chinese()
         }
         val animator = Animation.transfer(context!!, input_card, "translationY", -150f)
         val animator2 = Animation.transfer(context!!, input_card, "translationY", 0f)
@@ -69,41 +63,6 @@ class mainfragment : Fragment(){
             }
         }
     }
-
-    private fun lookup_chinese() {
-        //TODO 初始化
-        val text = tv_origin.text.toString()
-        if (TextUtils.isEmpty(text)) {
-            ToastUtils.show("请输入要查询的词")
-            return
-        }
-
-        if (chDictor == null) {
-            /*
-             * 请确保设置的路径下包含汉语词典的离线包
-             */
-            //            //设置离线句子库位置，确保Environment.getExternalStorageDirectory()+/update路径
-//            chDictor = ChDictor(Environment.getExternalStorageDirectory().toString() + "/update", false)
-
-            chDictor = ChDictor("dict", true)
-        }
-        try {
-            chDictor!!.init()
-        } catch (e: Exception) {
-            e.printStackTrace()
-        }
-
-        chDictor!!.lookup(text, object : DictListener {
-
-            override fun onResult(chTs: List<ChDictTranslate>) {
-                chTslate = chTs
-                showResult(chTs)
-            }
-
-            override fun onError(q: String, info: String) {
-
-            }
-        })    }
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
@@ -126,49 +85,4 @@ class mainfragment : Fragment(){
                 }
     }
 
-
-    fun showResult(chTs: List<ChDictTranslate>) {
-
-        var text = "查询失败"
-        val sb = StringBuilder()
-        for (chT in chTs) {
-            if (chT.success()) {
-                sb.append("<p>单词：" + chT.query + "<br/>")
-                sb.append("<p>发音：" + chT.phone + "<br/>")
-                val list = chT.translations
-                if (list != null) {
-                    sb.append("<p>含义如下：")
-                    for (chdictMeans in list) {
-                        val trs = chdictMeans.translate
-                        sb.append("<br/><br/>解释：" + trs
-                                + "<br/>例句：")
-                        val lineList = chdictMeans
-                                .examLines ?: continue
-                        for (line in lineList) {
-                            val texts = line.text
-                            var span: String? = null
-                            if (line.isHighlight) {
-                                span = ("<span><font color=\"#ff0000\">"
-                                        + texts
-                                        + "</font></span>")
-                            } else {
-                                span = ("<span><font color=\"#808080\">"
-                                        + texts
-                                        + "</font></span>")
-                            }
-                            sb.append(span)
-                        }
-                    }
-                }
-                sb.append("</p>")
-            }
-        }
-        text = sb.toString()
-        activity!!.runOnUiThread {
-            tv_target.text = text
-            more.visibility = View.VISIBLE
-        }
-
-
-    }
 }
